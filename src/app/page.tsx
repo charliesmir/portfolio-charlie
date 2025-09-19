31d361915bb8d0ba7ecf6ef8032f67b9d1bc718c
GudokVlad-Pilot <vladislavpogudin.dev@gmail.com>
2025-09-19 22:41:29 +0300

    testing vercel deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@
 export default function Home() {
   return (
     <div className="Home page">
-      Hello World
+      <div>Testing Vercel deployment</div>
+      <div>This is the initial setup for staging and production</div>
     </div>
   );
 }
