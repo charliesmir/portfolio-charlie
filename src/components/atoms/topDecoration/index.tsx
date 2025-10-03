37bfb1e50ae39b5d9da433b00d1246fa92555fb2
Charlie Smirnova <charlie.smirnova@gmail.com>
2025-10-04 01:26:17 +0300

    Top design elements and design tokens added

---
+++ src/components/atoms/topDecoration/index.tsx
@@ -0,0 +1,11 @@
+
+import "./topDecoration.css";
+
+export const topDecoration = () => {
+  return (
+    <div className="topDecoration">
+      <div className="topDecorationLeft"></div>
+      <div className="topDecorationRight"></div>
+    </div>
+  );
+};
