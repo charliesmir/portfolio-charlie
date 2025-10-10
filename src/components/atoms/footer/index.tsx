3cfa1dd5612b1d77141b66903d53ffb0b553953d
Charlie Smirnova <charlie.smirnova@gmail.com>
2025-10-10 22:34:43 +0300

    Footer created

---
+++ src/components/atoms/footer/index.tsx
@@ -0,0 +1,29 @@
+import "./footer.css";
+
+export const Footer = () => {
+  return (
+    <div className="FooterBox">
+      <div className="FooterText">© Copyright 2025. All rights reserved.</div>
+      <div className="FooterText">
+        Created by&nbsp;
+        <a
+          className="FooterLinkText"
+          href="https://www.instagram.com/charlieandarchitecture"
+          target="blank"
+          rel="noopener noreferrer"
+        >
+          Charlie
+        </a>
+        <div>&nbsp;&&nbsp;</div>
+        <a
+          className="FooterLinkText"
+          href="https://gudokvlad.com/"
+          target="blank"
+          rel="noopener noreferrer"
+        >
+          GudokVlad
+        </a>
+      </div>
+    </div>
+  );
+};
