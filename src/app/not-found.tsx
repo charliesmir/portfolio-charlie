b4aba18f563242c36ea9036a49022fbebb8eeea9
Charlie Smirnova <charlie.smirnova@gmail.com>
2025-10-15 14:55:03 +0300

    Small fixes

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,15 +3,24 @@ import "./notFopundPage.css";
 import { BackHomeButtonProps } from "@/components/atoms/backHomeButton";
 import { Footer } from "@/components/atoms/footer";
 import { NotFoundSection } from "@/components/molecules/notFoundSection";
+import { useRouter } from "next/navigation";
 
 interface Props {
   button: BackHomeButtonProps;
 }
 
 export default function GlobalNotFound({ button }: Props) {
+  // Basic settings
+  const router = useRouter();
   return (
     <div className="NFPage">
-      <NotFoundSection button={button} />
+      <NotFoundSection
+        button={
+          (button = {
+            onClick: () => router.push("/"),
+          })
+        }
+      />
       <div className="NFFooter">
         <Footer />
       </div>
