08f07b609cf0e0c37ffd5da3b08e9df1d9d5b81c
Charlie Smirnova <charlie.smirnova@gmail.com>
2025-10-28 14:51:34 +0200

    Arrows for about cards functional

--- src/components/molecules/mobileAboutCardsCarousel/index.tsx
+++ src/components/molecules/mobileAboutCardsCarousel/index.tsx
@@ -1,6 +1,7 @@
 import "./mobileAboutCardsCarousel.css";
 import { useRef } from "react";
 import { AboutCard, AboutCardProps } from "@/components/atoms/aboutCard";
+import { ArrowLong } from "@/components/atoms/arrowLong";
 
 interface MobileAboutCardsProps {
   /** About cards */
@@ -9,12 +10,29 @@ interface MobileAboutCardsProps {
 
 export const MobileAboutCardsCarousel = ({ cards }: MobileAboutCardsProps) => {
   const carouselRef = useRef<HTMLDivElement>(null);
+  const handlePrev = () => {
+    if (carouselRef.current) {
+      carouselRef.current.scrollBy({ left: -360, behavior: "smooth" });
+    }
+  };
+
+  const handleNext = () => {
+    if (carouselRef.current) {
+      carouselRef.current.scrollBy({ left: 360, behavior: "smooth" });
+    }
+  };
   return (
     <div className="MobileAboutCardsCarouselBox">
       <div className="MobileAboutCardsCarouselCards" ref={carouselRef}>
         {cards.map((card, index) => (
           <AboutCard key={index} {...card} />
         ))}
+      </div>
+      <div className="MobileAboutCardsCarouselButton prevButton">
+        <ArrowLong direction={"left"} onClick={handlePrev} />
+      </div>
+      <div className="MobileAboutCardsCarouselButton nextButton">
+        <ArrowLong direction={"right"} onClick={handleNext} />
       </div>
     </div>
   );
