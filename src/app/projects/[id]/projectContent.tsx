81cc57f7afa9090444192a9e267e342344a41540
GudokVlad-Pilot <vladislavpogudin.dev@gmail.com>
2025-10-28 22:55:54 +0200

    project page is having loader

--- src/app/projects/[id]/projectContent.tsx
+++ src/app/projects/[id]/projectContent.tsx
@@ -3,9 +3,11 @@
 import "./projectPage.css";
 import { SanityProjectPage } from "@/adapters/types";
 import { Footer } from "@/components/atoms/footer";
+import { LoaderAnimation } from "@/components/atoms/loaderAnimation";
 import { ProjectPageTopSection } from "@/components/molecules/projectPageTopSection";
 import { ProjectPageBottomSection } from "@/components/organisms/projectPageBottomSection";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 interface Props {
   project: SanityProjectPage;
@@ -15,12 +17,26 @@ export default function ProjectContent({ project }: Props) {
   // Basic settings
   const router = useRouter();
 
+  const [loading, setLoading] = useState<boolean>(false);
+
+  if (loading) {
+    return (
+      <div className="ProjectLoading">
+        <div className="ProjectLoadingText">Loading...</div>
+        <LoaderAnimation />
+      </div>
+    );
+  }
+
   return (
     <div className="ProjectPage">
       <ProjectPageTopSection
         flag={project.flag}
         button={{
-          onClick: () => router.push("/"),
+          onClick: () => {
+            setLoading(true);
+            router.push("/");
+          },
         }}
         image={project.primaryImage}
       />
