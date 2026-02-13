a60eba5aea255404f6f499d050cb0e4c16e12bd8
GudokVlad-Pilot <vladislavpogudin.dev@gmail.com>
2026-02-13 20:27:51 +0200

    home page revalidation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import { adapters } from "@/adapters/adapters";
 import HomeContent from "./homeContent";
 
+export const revalidate = 31536000; // 1 year
+
 const { getSanityMain } = adapters.cms();
 
 export default async function Page() {
